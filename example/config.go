f9192a46f7a5073c6821e05e1336a47e2fda765a
yangzhou10 <yangzhou10@iflytek.com>
2018-08-10 10:26:37 +0800

    对toml文件进行解析

--- example/config.go
+++ example/config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	common "git.xfyun.cn/AIaaS/finder-go/common"
+	"strings"
 )
 
 // ConfigChangedHandle ConfigChangedHandle
@@ -12,6 +13,9 @@ type ConfigChangedHandle struct {
 
 // OnConfigFileChanged OnConfigFileChanged
 func (s *ConfigChangedHandle) OnConfigFileChanged(config *common.Config) bool {
-	fmt.Println("收到 【", config.Name, " 】配置文件修改，最新内容如下 :\r\n", string(config.File))
-	return true
+	if strings.HasSuffix(config.Name,".toml") {
+		fmt.Println(config.Name, " has changed:\r\n", string(config.File)," \r\n 解析后的map为 ：",config.ConfigMap)
+	}else{
+		fmt.Println(config.Name, " has changed:\r\n", string(config.File))
+	}
 }
