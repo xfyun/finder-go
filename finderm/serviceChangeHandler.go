0cc2abbd4d23ca5e84105e7b9a83bc6b3d7dc1d0
sjliu7 <1581503100@qq.com>
2021-04-23 17:26:27 +0800

    update v3

--- finderm/serviceChangeHandler.go
+++ finderm/serviceChangeHandler.go
@@ -2,8 +2,8 @@ package finderm
 
 import (
 	"fmt"
-	common "git.iflytek.com/AIaaS/finder-go/common"
-	"git.iflytek.com/AIaaS/finder-go/log"
+	common "git.iflytek.com/AIaaS/finder-go/v3/common"
+	"git.iflytek.com/AIaaS/finder-go/v3/log"
 )
 
 type serviceChangeHandler struct {
